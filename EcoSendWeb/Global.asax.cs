a3846d4655187907a6133c7cbcf9cd1ddc9da354
Dmytro Skrypka <dmytro.sk@outlook.com>
2021-05-29 21:21:17 +0200

    clearing...

--- EcoSendWeb/Global.asax.cs
+++ EcoSendWeb/Global.asax.cs
@@ -1,11 +1,8 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
 using System.Web;
 using System.Web.Mvc;
 using System.Web.Optimization;
 using System.Web.Routing;
-using System.Web.Script.Serialization;
 using System.Web.Security;
 using EcoSendWeb.App_Start;
 using EcoSendWeb.Infrastructure;
@@ -33,46 +30,9 @@ namespace EcoSendWeb
             FormAuth.Init();
         }
 
-        private static readonly System.Text.RegularExpressions.Regex reMatchDomainName = new System.Text.RegularExpressions.Regex(@"^([\w\-]+\.)+((sk)|(cz)|(com)|(net)|(org)|(info))");
-
         protected void Application_BeginRequest(Object sender, EventArgs e)
         {
-            HttpApplication app = (HttpApplication)sender;
-            Uri uriSelf = app.Request.Url;
 
-            UriBuilder ubNewAddress = new UriBuilder(uriSelf);
-
-            //string strHostName = uriSelf.Host.ToLower();
-            //if (rgOfficialNames.Length > 0 && !rgOfficialNames.Any(x => x == strHostName) && reMatchDomainName.IsMatch(strHostName))
-            //{
-            //    string strTld = strHostName.Split('.')[0].ToLowerInvariant();
-            //    string strNewHost = rgOfficialNames.FirstOrDefault(q => q.Split('.')[0].ToLowerInvariant() == strTld);
-            //    if (strNewHost == null)
-            //    {
-            //        strNewHost = rgOfficialNames[0];
-            //    }
-
-            //    if (!app.Request.IsLocal && ubNewAddress.Scheme == "http")
-            //    {
-            //        ubNewAddress.Scheme = "https";
-            //        ubNewAddress.Port = -1;
-            //    }
-
-            //    ubNewAddress.Host = strNewHost;
-            //    app.Response.StatusCode = 301;
-            //    app.Response.AddHeader("Location", ubNewAddress.Uri.ToString());
-            //    app.Response.End();
-            //}
-            //else
-            //{
-            //    if (!app.Request.IsLocal && ubNewAddress.Scheme == "http")
-            //    {
-            //        ubNewAddress.Scheme = "https";
-            //        ubNewAddress.Port = -1;
-
-            //        app.Response.Redirect(ubNewAddress.Uri.ToString(), true);
-            //    }
-            //}
         }
 
         protected void Application_PostAuthenticateRequest(Object sender, EventArgs e)
